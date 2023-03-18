gangmuk/istio

commit 1fa3aa4835e63edb033ccbbdc358327faa039198

pilot: improve xds event testing (#43786)
* Ensure we actually fail when the event is unexpected
* Cleanup and consolidate some usage
* Fix a test that silently failed, taking 5s, due to missing and
  incorrect assertion

pilot/pkg/serviceregistry/kube/controller/fake.go
@@ -120,31 +120,15 @@ func (fx *FakeXdsUpdater) RemoveShard(shardKey model.ShardKey) {
 
 func (fx *FakeXdsUpdater) WaitOrFail(t test.Failer, et string) *FakeXdsEvent {
 	t.Helper()
-	return fx.WaitForDurationOrFail(t, et, 5*time.Second)
-}
-
-func (fx *FakeXdsUpdater) WaitForDurationOrFail(t test.Failer, et string, d time.Duration) *FakeXdsEvent {
-	t.Helper()
-	ev := fx.WaitForDuration(et, d)
-	if ev == nil {
-		t.Fatalf("Timeout creating %q after %s", et, d)
-	}
-	return ev
-}
-
-func (fx *FakeXdsUpdater) Wait(et string) *FakeXdsEvent {
-	return fx.WaitForDuration(et, 5*time.Second)
-}
-
-func (fx *FakeXdsUpdater) WaitForDuration(et string, d time.Duration) *FakeXdsEvent {
 	for {
 		select {
 		case e := <-fx.Events:
 			if e.Type == et {
 				return &e
 			}
+			log.Infof("skipping event %q want %q", e.Type, et)
 			continue
-		case <-time.After(d):
+		case <-time.After(time.Second * 5):
 			return nil
 		}
 	}
@@ -158,6 +142,23 @@ func (fx *FakeXdsUpdater) Clear() {
 		case <-fx.Events:
 		default:
 			wait = false
+		}
+	}
+}
+
+// AssertEmpty ensures there are no events in the channel
+func (fx *FakeXdsUpdater) AssertEmpty(t test.Failer, dur time.Duration) {
+	if dur == 0 {
+		select {
+		case e := <-fx.Events:
+			t.Fatalf("got unexpected event %+v", e)
+		default:
+		}
+	} else {
+		select {
+		case e := <-fx.Events:
+			t.Fatalf("got unexpected event %+v", e)
+		case <-time.After(dur):
 		}
 	}
 }

pilot/pkg/serviceregistry/kube/controller/controller_test.go
@@ -362,10 +362,7 @@ func TestGetProxyServiceInstances(t *testing.T) {
 					annotation.AlphaCanonicalServiceAccounts.Name:  canonicalSaOnVM,
 				},
 				[]int32{8080}, map[string]string{"app": "prod-app"}, t)
-			ev := fx.Wait("service")
-			if ev == nil {
-				t.Fatal("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
 
 			// Endpoints are generated by Kubernetes from pod labels and service selectors.
 			// Here we manually create them for mocking purpose.
@@ -384,15 +381,15 @@ func TestGetProxyServiceInstances(t *testing.T) {
 						annotation.AlphaCanonicalServiceAccounts.Name:  canonicalSaOnVM,
 					},
 					[]int32{8080}, map[string]string{"app": "prod-app"}, t)
-				fx.Wait("service")
+				fx.WaitOrFail(t, "service")
 
 				createEndpoints(t, controller, svcName, "nsfake", portNames, svc1Ips, nil, nil)
-				fx.Wait("eds")
+				fx.WaitOrFail(t, "eds")
 			}
 
 			// Create 1 endpoint that refers to a pod in the same namespace.
 			createEndpoints(t, controller, "svc1", "nsa", portNames, svc1Ips, nil, nil)
-			fx.Wait("eds")
+			fx.WaitOrFail(t, "eds")
 
 			// this can test get pod by proxy ID
 			svcNode := &model.Proxy{
@@ -857,10 +854,7 @@ func TestGetProxyServiceInstancesWithMultiIPsAndTargetPorts(t *testing.T) {
 					},
 					c.ports, map[string]string{"app": "test-app"}, t)
 
-				ev := fx.Wait("service")
-				if ev == nil {
-					t.Fatal("Timeout creating service")
-				}
+				fx.WaitOrFail(t, "service")
 				serviceInstances := controller.GetProxyServiceInstances(&model.Proxy{Metadata: &model.NodeMetadata{}, IPAddresses: c.ips})
 
 				for i, svc := range serviceInstances {
@@ -1275,15 +1269,11 @@ func TestController_ServiceWithFixedDiscoveryNamespaces(t *testing.T) {
 			createService(controller, "svc1", nsA,
 				map[string]string{},
 				[]int32{8080}, map[string]string{"test-app": "test-app-1"}, t)
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
 			createService(controller, "svc2", nsA,
 				map[string]string{},
 				[]int32{8081}, map[string]string{"test-app": "test-app-2"}, t)
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
 			// service event handlers should not trigger for svc3 and svc4
 			createService(controller, "svc3", nsB,
 				map[string]string{},
@@ -1301,12 +1291,8 @@ func TestController_ServiceWithFixedDiscoveryNamespaces(t *testing.T) {
 			// test updating namespace with adding discovery label
 			updateNamespace(t, controller.client.Kube(), nsB, map[string]string{"env": "test"})
 			// service event handlers should trigger for svc3 and svc4
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
+			fx.WaitOrFail(t, "service")
 			expectedSvcList = []*model.Service{svc1, svc2, svc3, svc4}
 			eventually(t, func() bool {
 				svcList := controller.Services()
@@ -1316,12 +1302,8 @@ func TestController_ServiceWithFixedDiscoveryNamespaces(t *testing.T) {
 			// test updating namespace by removing discovery label
 			updateNamespace(t, controller.client.Kube(), nsA, map[string]string{"pilot-discovery": "disabled"})
 			// service event handlers should trigger for svc1 and svc2
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
+			fx.WaitOrFail(t, "service")
 			expectedSvcList = []*model.Service{svc3, svc4}
 			eventually(t, func() bool {
 				svcList := controller.Services()
@@ -1392,9 +1374,7 @@ func TestController_ServiceWithChangingDiscoveryNamespaces(t *testing.T) {
 
 		// assert firing of service events
 		for i := 0; i < expectedNumSvcEvents; i++ {
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("timed out waiting for service event")
-			}
+			fx.WaitOrFail(t, "service")
 		}
 
 		eventually(t, func() bool {
@@ -1447,27 +1427,19 @@ func TestController_ServiceWithChangingDiscoveryNamespaces(t *testing.T) {
 			createService(controller, "svc1", nsA,
 				map[string]string{},
 				[]int32{8080}, map[string]string{"test-app": "test-app-1"}, t)
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
 			createService(controller, "svc2", nsA,
 				map[string]string{},
 				[]int32{8081}, map[string]string{"test-app": "test-app-2"}, t)
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
 			createService(controller, "svc3", nsB,
 				map[string]string{},
 				[]int32{8082}, map[string]string{"test-app": "test-app-3"}, t)
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
 			createService(controller, "svc4", nsC,
 				map[string]string{},
 				[]int32{8083}, map[string]string{"test-app": "test-app-4"}, t)
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
 
 			expectedSvcList := []*model.Service{svc1, svc2, svc3, svc4}
 			eventually(t, func() bool {
@@ -1610,9 +1582,7 @@ func TestControllerEnableResourceScoping(t *testing.T) {
 
 		// assert firing of service events
 		for i := 0; i < expectedNumSvcEvents; i++ {
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("timed out waiting for service event")
-			}
+			fx.WaitOrFail(t, "service")
 		}
 
 		eventually(t, func() bool {
@@ -1661,27 +1631,19 @@ func TestControllerEnableResourceScoping(t *testing.T) {
 	createService(controller, "svc1", nsA,
 		map[string]string{},
 		[]int32{8080}, map[string]string{"test-app": "test-app-1"}, t)
-	if ev := fx.Wait("service"); ev == nil {
-		t.Fatal("Timeout creating service")
-	}
+	fx.WaitOrFail(t, "service")
 	createService(controller, "svc2", nsA,
 		map[string]string{},
 		[]int32{8081}, map[string]string{"test-app": "test-app-2"}, t)
-	if ev := fx.Wait("service"); ev == nil {
-		t.Fatal("Timeout creating service")
-	}
+	fx.WaitOrFail(t, "service")
 	createService(controller, "svc3", nsB,
 		map[string]string{},
 		[]int32{8082}, map[string]string{"test-app": "test-app-3"}, t)
-	if ev := fx.Wait("service"); ev == nil {
-		t.Fatal("Timeout creating service")
-	}
+	fx.WaitOrFail(t, "service")
 	createService(controller, "svc4", nsC,
 		map[string]string{},
 		[]int32{8083}, map[string]string{"test-app": "test-app-4"}, t)
-	if ev := fx.Wait("service"); ev == nil {
-		t.Fatal("Timeout creating service")
-	}
+	fx.WaitOrFail(t, "service")
 
 	expectedSvcList := []*model.Service{svc1, svc2, svc3, svc4}
 	eventually(t, func() bool {
@@ -1708,9 +1670,7 @@ func TestControllerEnableResourceScoping(t *testing.T) {
 	)
 
 	// namespace nsB, nsC deselected
-	if ev := fx.Wait("xds"); ev == nil {
-		t.Fatal("Timeout waiting xds")
-	}
+	fx.WaitOrFail(t, "xds")
 
 	// create vs1 in nsA
 	createVirtualService(controller, "vs1", nsA, map[string]string{}, t)
@@ -1741,9 +1701,7 @@ func TestControllerEnableResourceScoping(t *testing.T) {
 	)
 
 	// namespace nsB selected
-	if ev := fx.Wait("xds"); ev == nil {
-		t.Fatal("Timeout waiting xds")
-	}
+	fx.WaitOrFail(t, "xds")
 }
 
 func TestInstancesByPort_WorkloadInstances(t *testing.T) {
@@ -2314,7 +2272,7 @@ func addPods(t *testing.T, controller *FakeController, fx *FakeXdsUpdater, pods 
 			t.Fatal(err)
 		}
 		// pod first time occur will trigger proxy push
-		fx.Wait("proxy")
+		fx.WaitOrFail(t, "proxy")
 	}
 }
 
@@ -2408,9 +2366,7 @@ func TestEndpointUpdate(t *testing.T) {
 			// 1. incremental eds for normal service endpoint update
 			createService(controller, "svc1", "nsa", nil,
 				[]int32{8080}, map[string]string{"app": "prod-app"}, t)
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatal("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
 
 			// Endpoints are generated by Kubernetes from pod labels and service selectors.
 			// Here we manually create them for mocking purpose.
@@ -2418,35 +2374,26 @@ func TestEndpointUpdate(t *testing.T) {
 			portNames := []string{"tcp-port"}
 			// Create 1 endpoint that refers to a pod in the same namespace.
 			createEndpoints(t, controller, "svc1", "nsa", portNames, svc1Ips, nil, nil)
-			if ev := fx.Wait("eds"); ev == nil {
-				t.Fatalf("Timeout incremental eds")
-			}
+			fx.WaitOrFail(t, "eds")
 
 			// delete normal service
 			err := controller.client.Kube().CoreV1().Services("nsa").Delete(context.TODO(), "svc1", metav1.DeleteOptions{})
 			if err != nil {
 				t.Fatalf("Cannot delete service (error: %v)", err)
 			}
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatalf("Timeout deleting service")
-			}
+			fx.WaitOrFail(t, "service")
 
 			// 2. full xds push request for headless service endpoint update
 
 			// create a headless service
 			createServiceWithoutClusterIP(controller, "svc1", "nsa", nil,
 				[]int32{8080}, map[string]string{"app": "prod-app"}, t)
-			if ev := fx.Wait("service"); ev == nil {
-				t.Fatalf("Timeout creating service")
-			}
+			fx.WaitOrFail(t, "service")
 
 			// Create 1 endpoint that refers to a pod in the same namespace.
 			svc1Ips = append(svc1Ips, "128.0.0.2")
 			updateEndpoints(controller, "svc1", "nsa", portNames, svc1Ips, t)
-			ev := fx.Wait("xds")
-			if ev == nil {
-				t.Fatalf("Timeout xds push")
-			}
+			ev := fx.WaitOrFail(t, "xds")
 			if ev.ID != string(kube.ServiceHostname("svc1", "nsa", controller.opts.DomainSuffix)) {
 				t.Errorf("Expect service %s updated, but got %s",
 					kube.ServiceHostname("svc1", "nsa", controller.opts.DomainSuffix), ev.ID)
@@ -2485,9 +2432,7 @@ func TestEndpointUpdateBeforePodUpdate(t *testing.T) {
 				// create service
 				createService(controller, name, "nsA", nil,
 					[]int32{8080}, map[string]string{"app": "prod-app"}, t)
-				if ev := fx.Wait("service"); ev == nil {
-					t.Fatal("Timeout creating service")
-				}
+				fx.WaitOrFail(t, "service")
 			}
 			addEndpoint := func(svcName string, ips []string, pods []string) {
 				var refs []*corev1.ObjectReference
@@ -2506,10 +2451,7 @@ func TestEndpointUpdateBeforePodUpdate(t *testing.T) {
 			}
 			assertEndpointsEvent := func(ips []string, pods []string) {
 				t.Helper()
-				ev := fx.Wait("eds")
-				if ev == nil {
-					t.Fatalf("Timeout incremental eds")
-				}
+				ev := fx.WaitOrFail(t, "eds")
 				var gotIps []string
 				for _, e := range ev.Endpoints {
 					gotIps = append(gotIps, e.Address)
@@ -2623,15 +2565,11 @@ func TestWorkloadInstanceHandlerMultipleEndpoints(t *testing.T) {
 	addPods(t, controller, fx, pods...)
 	createService(controller, "svc1", "nsA", nil,
 		[]int32{8080}, map[string]string{"app": "prod-app"}, t)
-	if ev := fx.Wait("service"); ev == nil {
-		t.Fatal("Timeout creating service")
-	}
+	fx.WaitOrFail(t, "service")
 	pod1Ips := []string{"172.0.1.1"}
 	portNames := []string{"tcp-port"}
 	createEndpoints(t, controller, "svc1", "nsA", portNames, pod1Ips, nil, nil)
-	if ev := fx.Wait("eds"); ev == nil {
-		t.Fatal("Timeout incremental eds")
-	}
+	fx.WaitOrFail(t, "eds")
 
 	// Simulate adding a workload entry (fired through invocation of WorkloadInstanceHandler)
 	controller.WorkloadInstanceHandler(&model.WorkloadInstance{
@@ -2646,25 +2584,22 @@ func TestWorkloadInstanceHandlerMultipleEndpoints(t *testing.T) {
 
 	expectedEndpointIPs := []string{"172.0.1.1", "2.2.2.2"}
 	// Check if an EDS event is fired
-	if ev := fx.Wait("eds"); ev == nil {
-		t.Fatal("Did not get eds event when workload entry was added")
-	} else {
-		// check if the hostname matches that of k8s service svc1.nsA
-		if ev.ID != "svc1.nsA.svc.company.com" {
-			t.Fatalf("eds event for workload entry addition did not match the expected service. got %s, want %s",
-				ev.ID, "svc1.nsA.svc.company.com")
-		}
-		// we should have the pod IP and the workload Entry's IP in the endpoints..
-		// the first endpoint should be that of the k8s pod and the second one should be the workload entry
+	ev := fx.WaitOrFail(t, "eds")
+	// check if the hostname matches that of k8s service svc1.nsA
+	if ev.ID != "svc1.nsA.svc.company.com" {
+		t.Fatalf("eds event for workload entry addition did not match the expected service. got %s, want %s",
+			ev.ID, "svc1.nsA.svc.company.com")
+	}
+	// we should have the pod IP and the workload Entry's IP in the endpoints..
+	// the first endpoint should be that of the k8s pod and the second one should be the workload entry
 
-		var gotEndpointIPs []string
-		for _, ep := range ev.Endpoints {
-			gotEndpointIPs = append(gotEndpointIPs, ep.Address)
-		}
-		if !reflect.DeepEqual(gotEndpointIPs, expectedEndpointIPs) {
-			t.Fatalf("eds update after adding workload entry did not match expected list. got %v, want %v",
-				gotEndpointIPs, expectedEndpointIPs)
-		}
+	var gotEndpointIPs []string
+	for _, ep := range ev.Endpoints {
+		gotEndpointIPs = append(gotEndpointIPs, ep.Address)
+	}
+	if !reflect.DeepEqual(gotEndpointIPs, expectedEndpointIPs) {
+		t.Fatalf("eds update after adding workload entry did not match expected list. got %v, want %v",
+			gotEndpointIPs, expectedEndpointIPs)
 	}
 
 	// Check if InstancesByPort returns the same list
@@ -2673,7 +2608,7 @@ func TestWorkloadInstanceHandlerMultipleEndpoints(t *testing.T) {
 		t.Fatalf("failed to get services (%v), converted", converted)
 	}
 	instances := controller.InstancesByPort(converted[0], 8080)
-	var gotEndpointIPs []string
+	gotEndpointIPs = []string{}
 	for _, instance := range instances {
 		gotEndpointIPs = append(gotEndpointIPs, instance.Endpoint.Address)
 	}
@@ -2684,18 +2619,15 @@ func TestWorkloadInstanceHandlerMultipleEndpoints(t *testing.T) {
 
 	// Now add a k8s pod to the service and ensure that eds updates contain both pod IPs and workload entry IPs.
 	updateEndpoints(controller, "svc1", "nsA", portNames, []string{"172.0.1.1", "172.0.1.2"}, t)
-	if ev := fx.Wait("eds"); ev == nil {
-		t.Fatal("Timeout incremental eds")
-	} else {
-		var gotEndpointIPs []string
-		for _, ep := range ev.Endpoints {
-			gotEndpointIPs = append(gotEndpointIPs, ep.Address)
-		}
-		expectedEndpointIPs = []string{"172.0.1.1", "172.0.1.2", "2.2.2.2"}
-		if !reflect.DeepEqual(gotEndpointIPs, expectedEndpointIPs) {
-			t.Fatalf("eds update after adding pod did not match expected list. got %v, want %v",
-				gotEndpointIPs, expectedEndpointIPs)
-		}
+	ev = fx.WaitOrFail(t, "eds")
+	gotEndpointIPs = []string{}
+	for _, ep := range ev.Endpoints {
+		gotEndpointIPs = append(gotEndpointIPs, ep.Address)
+	}
+	expectedEndpointIPs = []string{"172.0.1.1", "172.0.1.2", "2.2.2.2"}
+	if !reflect.DeepEqual(gotEndpointIPs, expectedEndpointIPs) {
+		t.Fatalf("eds update after adding pod did not match expected list. got %v, want %v",
+			gotEndpointIPs, expectedEndpointIPs)
 	}
 }
 
@@ -2814,16 +2746,12 @@ func TestUpdateEdsCacheOnServiceUpdate(t *testing.T) {
 	addPods(t, controller, fx, pods...)
 	createService(controller, "svc1", "nsA", nil,
 		[]int32{8080}, map[string]string{"app": "prod-app"}, t)
-	if ev := fx.Wait("service"); ev == nil {
-		t.Fatal("Timeout creating service")
-	}
+	fx.WaitOrFail(t, "service")
 
 	pod1Ips := []string{"172.0.1.1"}
 	portNames := []string{"tcp-port"}
 	createEndpoints(t, controller, "svc1", "nsA", portNames, pod1Ips, nil, nil)
-	if ev := fx.Wait("eds"); ev == nil {
-		t.Fatal("Timeout incremental eds")
-	}
+	fx.WaitOrFail(t, "eds")
 
 	// update service selector
 	svc := getService(controller, "svc1", "nsA", t)
@@ -2839,9 +2767,8 @@ func TestUpdateEdsCacheOnServiceUpdate(t *testing.T) {
 	}()
 	svc = updateService(controller, svc, t)
 	// don't update eds cache if `K8S_SELECT_WORKLOAD_ENTRIES` is disabled
-	if ev := fx.Wait("eds cache"); ev != nil {
-		t.Fatal("Update eds cache unexpectedly")
-	}
+	fx.WaitOrFail(t, "service")
+	fx.AssertEmpty(t, 0)
 
 	features.EnableK8SServiceSelectWorkloadEntries = true
 	svc.Spec.Selector = map[string]string{
@@ -2849,9 +2776,7 @@ func TestUpdateEdsCacheOnServiceUpdate(t *testing.T) {
 	}
 	updateService(controller, svc, t)
 	// update eds cache if `K8S_SELECT_WORKLOAD_ENTRIES` is enabled
-	if ev := fx.Wait("eds cache"); ev == nil {
-		t.Fatal("Timeout updating eds cache")
-	}
+	fx.WaitOrFail(t, "eds cache")
 }
 
 func TestDiscoverySelector(t *testing.T) {

pilot/pkg/config/kube/gateway/controller_test.go
@@ -190,8 +190,6 @@ func TestListVirtualServiceResourceType(t *testing.T) {
 }
 
 func TestNamespaceEvent(t *testing.T) {
-	g := NewWithT(t)
-
 	clientSet := kube.NewFakeClient()
 	store := memory.NewController(memory.Make(collections.All))
 	c := NewController(clientSet, store, AlwaysReady, nil, controller.Options{})
@@ -224,32 +222,32 @@ func TestNamespaceEvent(t *testing.T) {
 	}}
 
 	clientSet.Kube().CoreV1().Namespaces().Create(ctx, &ns1, metav1.CreateOptions{})
-	g.Expect(s.WaitForDuration("xds", time.Second)).To(BeNil())
+	s.AssertEmpty(t, time.Millisecond*10)
 
 	clientSet.Kube().CoreV1().Namespaces().Create(ctx, &ns2, metav1.CreateOptions{})
-	g.Expect(s.WaitForDuration("xds", time.Second)).ToNot(BeNil())
+	s.WaitOrFail(t, "xds")
 
 	ns1.Annotations = map[string]string{"foo": "bar"}
 	clientSet.Kube().CoreV1().Namespaces().Update(ctx, &ns1, metav1.UpdateOptions{})
-	g.Expect(s.WaitForDuration("xds", time.Second)).To(BeNil())
+	s.AssertEmpty(t, time.Millisecond*10)
 
 	ns2.Annotations = map[string]string{"foo": "bar"}
 	clientSet.Kube().CoreV1().Namespaces().Update(ctx, &ns2, metav1.UpdateOptions{})
-	g.Expect(s.WaitForDuration("xds", time.Second)).To(BeNil())
+	s.AssertEmpty(t, time.Millisecond*10)
 
 	ns1.Labels["bar"] = "foo"
 	clientSet.Kube().CoreV1().Namespaces().Update(ctx, &ns1, metav1.UpdateOptions{})
-	g.Expect(s.WaitForDuration("xds", time.Second)).To(BeNil())
+	s.AssertEmpty(t, time.Millisecond*10)
 
 	ns2.Labels["foo"] = "bar"
 	clientSet.Kube().CoreV1().Namespaces().Update(ctx, &ns2, metav1.UpdateOptions{})
-	g.Expect(s.WaitForDuration("xds", time.Second)).ToNot(BeNil())
+	s.WaitOrFail(t, "xds")
 
 	ns1.Labels["allowed"] = "true"
 	clientSet.Kube().CoreV1().Namespaces().Update(ctx, &ns1, metav1.UpdateOptions{})
-	g.Expect(s.WaitForDuration("xds", time.Second)).ToNot(BeNil())
+	s.WaitOrFail(t, "xds")
 
 	ns2.Labels["allowed"] = "false"
 	clientSet.Kube().CoreV1().Namespaces().Update(ctx, &ns2, metav1.UpdateOptions{})
-	g.Expect(s.WaitForDuration("xds", time.Second)).ToNot(BeNil())
+	s.WaitOrFail(t, "xds")
 }

pilot/pkg/serviceregistry/kube/controller/endpointslice_test.go
@@ -51,9 +51,7 @@ func TestEndpointSliceFromMCSShouldBeIgnored(t *testing.T) {
 
 	createService(controller, svcName, ns, nil,
 		[]int32{8080}, map[string]string{"app": appName}, t)
-	if ev := fx.Wait("service"); ev == nil {
-		t.Fatal("Timeout creating service")
-	}
+	fx.WaitOrFail(t, "service")
 
 	// Ensure that the service is available.
 	hostname := kube.ServiceHostname(svcName, ns, controller.opts.DomainSuffix)
@@ -68,9 +66,7 @@ func TestEndpointSliceFromMCSShouldBeIgnored(t *testing.T) {
 	createEndpoints(t, controller, svcName, ns, portNames, svc1Ips, nil, map[string]string{
 		mcs.LabelServiceName: svcName,
 	})
-	if ev := fx.WaitForDuration("eds", 200*time.Millisecond); ev != nil {
-		t.Fatalf("Received unexpected EDS event")
-	}
+	fx.AssertEmpty(t, time.Millisecond*50)
 
 	// Ensure that getting by port returns no ServiceInstances.
 	instances := controller.InstancesByPort(svc, svc.Ports[0].Port)

pilot/pkg/serviceregistry/kube/controller/serviceexportcache_test.go
@@ -16,7 +16,6 @@ package controller
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"testing"
 
@@ -202,10 +201,7 @@ func (ec *serviceExportCacheImpl) unExport(t *testing.T) {
 func (ec *serviceExportCacheImpl) waitForXDS(t *testing.T, exported bool) {
 	t.Helper()
 	retry.UntilSuccessOrFail(t, func() error {
-		event := ec.opts.XDSUpdater.(*FakeXdsUpdater).Wait("eds")
-		if event == nil {
-			return errors.New("failed waiting for XDS event")
-		}
+		event := ec.opts.XDSUpdater.(*FakeXdsUpdater).WaitOrFail(t, "eds")
 		if len(event.Endpoints) != 1 {
 			return fmt.Errorf("waitForXDS failed: expected 1 endpoint, found %d", len(event.Endpoints))
 		}

pilot/pkg/serviceregistry/kube/controller/serviceimportcache_test.go
@@ -16,7 +16,6 @@ package controller
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"reflect"
 	"testing"
@@ -440,7 +439,7 @@ func (ic *serviceImportCacheImpl) createServiceImport(t *testing.T, importType m
 
 	if shouldCreateMCSService {
 		// Wait for the XDS event.
-		ic.waitForXDS(t)
+		ic.checkXDS(t)
 	}
 }
 
@@ -473,7 +472,7 @@ func (ic *serviceImportCacheImpl) setServiceImportVIPs(t *testing.T, vips []stri
 		}, serviceImportTimeout)
 
 		// Wait for the XDS event.
-		ic.waitForXDS(t)
+		ic.checkXDS(t)
 	} else {
 		// Wait for the import to be processed by the controller.
 		retry.UntilSuccessOrFail(t, func() error {
@@ -510,19 +509,9 @@ func (ic *serviceImportCacheImpl) isImported(name types.NamespacedName) bool {
 	return item != nil
 }
 
-func (ic *serviceImportCacheImpl) waitForXDS(t *testing.T) {
+func (ic *serviceImportCacheImpl) checkXDS(t test.Failer) error {
 	t.Helper()
-
-	retry.UntilSuccessOrFail(t, func() error {
-		return ic.checkXDS()
-	}, serviceImportTimeout)
-}
-
-func (ic *serviceImportCacheImpl) checkXDS() error {
-	event := ic.opts.XDSUpdater.(*FakeXdsUpdater).Wait("service")
-	if event == nil {
-		return errors.New("failed waiting for XDS event")
-	}
+	event := ic.opts.XDSUpdater.(*FakeXdsUpdater).WaitOrFail(t, "service")
 
 	// The name of the event will be the cluster-local hostname.
 	eventID := serviceImportClusterSetHost.String()

commit 0bd7b851f6370e4464052678ad5983b3bd6230df

controller: properly fail on timeout (#43994)
This got broken in
https://github.com/istio/istio/commit/1fa3aa4835e63edb033ccbbdc358327faa039198

pilot/pkg/serviceregistry/kube/controller/fake.go
@@ -131,7 +131,7 @@ func (fx *FakeXdsUpdater) WaitOrFail(t test.Failer, et string) *FakeXdsEvent {
 			log.Infof("skipping event %q want %q", e.Type, et)
 			continue
 		case <-time.After(time.Second * 5):
-			return nil
+			t.Fatalf("timed out waiting for %v", et)
 		}
 	}
 }

pilot/pkg/serviceregistry/kube/controller/controller_test.go
@@ -1301,7 +1301,6 @@ func TestController_ServiceWithFixedDiscoveryNamespaces(t *testing.T) {
 			expectedSvcList = []*model.Service{svc3, svc4}
 			eventually(t, func() bool {
 				svcList := controller.Services()
-				log.Errorf("howardjohn: %v\n%v", svcList, expectedSvcList)
 				return servicesEqual(svcList, expectedSvcList)
 			})
 		})