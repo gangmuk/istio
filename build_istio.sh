86b4acfeb2f224451a4a916bdfd07b577dcabbc1
gangmuk@gmail.com <gangmuk@gmail.com>
2024-08-29 23:34:19 +0000

    Separate build and installation

--- build_istio.sh
+++ build_istio.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo cp ../proxy/bazel-out/k8-opt/bin/envoy out/linux_amd64/release/
 pat=$1
 
 docker login ghcr.io -u gangmuk -p ${pat}
@@ -11,9 +10,7 @@ sudo cp ../proxy/build-output/envoy-gangmuk/bin/envoy ./out/linux_amd64/release/
 sudo make docker.proxyv2
 sudo make push.docker.proxyv2 # This one requires docker login
 
-
 ## These two lines are not required again if you did it before
 # sudo make docker.pilot
 # sudo docker push docker.io/gangmuk/pilot:latest
 
-go run ./istioctl/cmd/istioctl install --set hub=docker.io/gangmuk  --set tag=latest -y
