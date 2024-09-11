15d076de74e5eeb7abdf4fd01db1fa4b1e17ecd8
gangmuk@gmail.com <gangmuk@gmail.com>
2024-09-11 20:46:52 +0000

    [WORKING], use build_istio.sh in the host

--- build_istio.sh
+++ build_istio.sh
@@ -1,19 +1,56 @@
 #!/bin/bash
 
-docker login ghcr.io -u ${github_id} -p ${PAT}
+build_output_dir="build-output/gangmuk-envoy"
+echo "project_repo: ${project_repo}"
+echo "build_output_dir: ${build_output_dir}"
 
-echo ${docker_pw} | docker login --username ${docker_id} --password-stdin && docker push gangmuk/proxyv2:latest
+if [ -z "${project_repo}" ]; then
+  echo "project_repo is empty"
+  echo "exit..."
+  exit
+fi
+if [ -z "${build_output_dir}" ]; then
+  echo "build_output_dir is empty"
+  echo "exit..."
+  exit
+fi
+if [ -z "${docker_id}" ]; then
+  echo "docker_id is empty"
+  echo "exit..."
+  exit
+fi
+if [ -z "${docker_pw}" ]; then
+  echo "docker_pw is empty"
+  echo "exit..."
+  exit
+fi
+echo "Will start in 5 seconds"
+sleep 5
 
-sudo make docker
-sudo make build
+echo ${docker_pw} | docker login --username ${docker_id} --password-stdin
+echo "****** Docker login"
 
-# project_repo="/proj/istio-PG0/projects"
-sudo cp ${project_repo}/proxy/build-output/envoy-gangmuk/bin/envoy ./out/linux_amd64/release/
+cd ${project_repo}/istio && \
+sudo make docker && \
+sudo make build &&
+echo "****** Istio make docker and make build"
 
-sudo make docker.proxyv2
-sudo make push.docker.proxyv2 # This one requires docker login
+## Copy custom envoy binary to istio out directory
+sudo cp ${project_repo}/proxy/${build_output_dir}/envoy ${project_repo}/istio/out/linux_amd64/release/
+# sudo cp ${project_repo}/proxy/${build_output_dir}/bin/envoy ${project_repo}/istio/out/linux_amd64/release/
+echo "****** Copy Envoy binary to Istio out directory"
 
-## These two lines are not required again if you did it before
-# sudo make docker.pilot
-# sudo docker push docker.io/gangmuk/pilot:latest
+## Push
+cd ${project_repo}/istio && \
+sudo make docker.proxyv2 && \
+sudo make push.docker.proxyv2
+echo "****** Istio with the new envoy is built and pushed"
 
+## Install
+cd ${project_repo}/istio && \
+go run ${PWD}/istioctl/cmd/istioctl install --set hub=docker.io/${docker_id}  --set tag=latest -y
+echo "****** Istio is installed"
+
+echo "****** Restart everything!"
+krrdistio
+krrd
